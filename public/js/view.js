ea6ae0132bbca76cb96534b26d5329b6147d7e63
Anna Vitali - anna.vitali4@studio.unibo.it <anna.vitali4@studio.unibo.it>
2023-05-11 09:49:14 +0200

    feat: create template for babylon.js and croquet.io application

---
+++ public/js/view.js
@@ -0,0 +1,18 @@
+class RootView extends Croquet.View {
+
+    /**
+    * Constructor for the class.
+    * @param {any} model the model of reference
+    */
+    constructor(model) {
+        super(model);
+        this.model = model;
+
+        console.log("VIEW subscribed ");
+        this.#addChangeColorMenu();
+    }
+}
+
+
+
+export { RootView };
